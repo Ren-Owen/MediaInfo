f24fb5ca148ad5cb44288023151c89c83e28ca3e
wangtiantian <wangtiantian@dongao.com>
2017-03-29 18:08:55 +0800

    Debug: edit version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MediaInfo',
-    version='0.0.4',
+    version='0.0.5',
 
     description='A Python wrapper around the ffprobe or mediainfo command line utility',
     long_description=open('README.rst').read(),
